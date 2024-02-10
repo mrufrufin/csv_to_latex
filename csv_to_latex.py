b4ceef6bc64b0771d349b3af19b53824afebfa0d
derek kwan 關志豪 <derek.x.kwan@gmail.com>
2024-02-10 13:59:44 -0800

    add start to subtables

--- csv_to_latex.py
+++ csv_to_latex.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import csv
 import argparse
 import os
@@ -8,15 +9,17 @@ FDIR = os.path.split(__file__)[0]
 DEFOUTF = os.path.join(FDIR, "out.txt")
 DEFINF = os.path.join(FDIR, "in.csv")
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("--i", type=str, default=DEFINF, help="input file")
-parser.add_argument("--vert", type=strtobool, default=True, help="vertical")
+parser.add_argument("-i", "--input", type=str, default=DEFINF, help="input file")
+parser.add_argument("-v", "--vert", type=strtobool, default=True, help="vertical")
+parser.add_argument("-s", "--save", type=strtobool, default=True, help="to save")
+parser.add_argument("-st", "--subtable", type=strtobool, default=False, help="make as subtable")
 parser.add_argument("--entrypos", type=str, default="l", help="entry position (l,c,r)")
 parser.add_argument("--center", type=strtobool, default=True, help="center table")
 parser.add_argument("--capheader", type=strtobool, default=True, help="capitalize header")
 
 args = parser.parse_args()
-
-infile = args.i
+tabwidth = 0.5
+infile = args.input
 fname = os.path.splitext(infile)[0]
 outfile = fname + ".txt"
 fname_p = re.sub(r'_', ' ', fname).title()
@@ -35,9 +38,22 @@ with open(infile, 'r', newline='') as csvr:
 
 num_col = len(header)
 num_rows = len(res[header[0]])
-rstr += "\\begin{table}[ht]\n"
+if args.subtable == False:
+    rstr += "\\begin{table}[ht]\n"
+else:
+    rstr += "\\begin{table*}[ht]\n"
+    if args.center == True:
+        rstr += "\\centering\n"
+if args.subtable == True:
+    rstr+= "\\captionsetup[subtable]{justification=centering,position = top}\n"
+    rstr += "\\captionsetup[table]{position = top}\n"
 rstr += f"\\caption{{{fname_p}}}\n"
 rstr += f"\\label{{{fname}}}\n"
+if args.subtable == True:
+        rstr += f"\\begin{{subtable}}[t]{{ {tabwidth}\\textwidth }}\n"
+        rstr += f"\\caption{{{fname_p}}}\n"
+        rstr += f"\label{{{fname}}}\n"
+
 if args.center == True:
     rstr += "\\centering\n"
 rstr += "\\begin{tabular}{"
@@ -91,9 +107,14 @@ else:
             rstr += "\\midrule\n"
 
 rstr+= "\\end{tabular}\n"
-rstr += "\\end{table}\n"
+if args.subtable == False:
+    rstr += "\\end{table}\n"
+else:
+    rstr += "\\end{subtable}\n"
+    rstr += "\\end{table*}\n"
 
-print(rstr)
+print(f"\n{rstr}")
 
-with open(outfile, "w") as f:
-    f.write(rstr)
+if args.save == True:
+    with open(outfile, "w") as f:
+        f.write(rstr)
